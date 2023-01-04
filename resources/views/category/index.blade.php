4721e65e6814b5eab7107e821072e7cfafb009c9
Herry Prasetyo <herry081288@gmail.com>
2023-01-04 08:34:17 +0700

    first commit

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Category Page') }}</div>
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nama Category</th>
+                                    <th>Tools</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
